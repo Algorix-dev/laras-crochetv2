fa1093204c7507cc0507467c832866f314f74cc4
Emmanuel (Algorix-dev) <emmanuel@algorix-dev.local>
2026-09-26 07:18:52 +0100

    fix: adjust bottom positioning of HeroModel and HeroCarousel components for improved alignment

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -686,7 +686,7 @@ function HeroModel({
       }}
       className="
         absolute
-        bottom-[2%]
+        bottom-0
         max-sm:bottom-[-7%]
         left-1/2
         flex
@@ -1073,7 +1073,7 @@ function HeroCarousel({ models }) {
               absolute
               left-1/2
               -translate-x-1/2 sm:mb-4
-              bottom-0  
+              bottom-[-5%]  
               max-sm:bottom-[-12%]
 
               z-0
