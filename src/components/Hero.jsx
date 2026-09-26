50342d1f1caa1462fa68530e330d62aa12d8f409
Emmanuel (Algorix-dev) <emmanuel@algorix-dev.local>
2026-09-26 06:43:14 +0100

    fix: adjust positioning of HeroCarousel component for improved layout

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1333,7 +1333,7 @@ function HeroCarousel({ models }) {
               absolute
               left-1/2
               -translate-x-1/2
-              bottom-0
+              top-full
               z-10
 
               mt-16 md:mt-18
